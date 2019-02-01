b3d73f0db7cf307780a55db33b458c89001a152c
Justin Horton <justinhorton@Justins-MacBook-Pro.local>
2019-02-01 16:11:08 -0700

    set up TaskCards initial

---
+++ src/component/LandingPage/LandingPage_TaskCards.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class LandingPage_TaskCards extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
