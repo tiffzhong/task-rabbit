fe1de9f14114db2613c33495267eafdd047aa60d
Justin Horton <justinhorton@Justins-MacBook-Pro.local>
2019-02-04 09:31:45 -0700

    initial taskCards

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './component/LandingPage/LandingPage.css';
+import LandingPage_TaskCards from './component/LandingPage/LandingPage_TaskCards';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <LandingPage_TaskCards />
       </div>
     );
   }
--- src/component/LandingPage/LandingPage_TaskCards.js
+++ src/component/LandingPage/LandingPage_TaskCards.js
@@ -3,8 +3,69 @@ import React, { Component } from 'react';
 export default class LandingPage_TaskCards extends Component {
     render() {
         return (
-            <div>
-                
+            <div className='TaskCards-component'>
+            TaskCards
+                <div className='taskCards-container'>
+                    <div className='container-wide'>
+                        <div className='row-thin'>
+                            <h2 className='static-title secondary-heading'>Popular Tasks</h2>
+                                <div className='carousel-container'>
+                                    <div className='carousel-navigate-prev'>
+                                        <i className='ss-lnr-chevron-left'>
+                                            
+                                        </i>
+                                    </div>
+                                    <div className='carousel-task-container'>
+                                        <span className='task'>
+                                            {/* <img />
+                                            <button></button> */}
+                                            <div></div>
+                                            <div>
+                                                <span>Hello</span>
+                                            </div>
+                                            <div></div>
+                                        </span>
+                                        <span className='task'>
+                                            <img />
+                                            <button></button>
+                                        </span>
+                                        <span className='task'>
+                                            <img />
+                                            <button></button>
+                                        </span>
+                                        <span className='task'>
+                                            <img />
+                                            <button></button>
+                                        </span>
+                                        <span className='task'>
+                                            <img />
+                                            <button></button>
+                                        </span>
+                                        <span className='task'>
+                                            <img />
+                                            <button></button>
+                                        </span>
+                                        <span className='task'>
+                                            <img />
+                                            <button></button>
+                                        </span>
+                                        <span className='task'>
+                                            <img />
+                                            <button></button>
+                                        </span>
+                                        <span className='task'>
+                                            <img />
+                                            <button></button>
+                                        </span>
+                                    </div>
+                                    <div className='carousel-navigate-next'>
+                                        <i className='chevron-right'></i>
+                                    </div>
+                                </div>
+                            
+                        </div> 
+                    </div>
+               </div>
             </div>
         );
     }
