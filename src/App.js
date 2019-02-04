a826c7c322902dbc4875cb673817bedb4f409f2a
Natalie Pulver <npulver00@gmail.com>
2019-02-04 09:38:10 -0700

    how it works

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 import NavBar_HowItWorks from "./component/NavBar/NavBar_HowItWorks";
-import LandingPage_Reviews from "./component/LandingPage/LandingPage_Reviews";
-
 import './App.css';
-import LandingPage_Reviews from './component/LandingPage/LandingPage_Reviews';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <LandingPage_Reviews />
+
         <NavBar_HowItWorks />
       </div>
     );
