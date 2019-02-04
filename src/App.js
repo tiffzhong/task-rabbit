e7126c807af33263a01cb2402564748e54869132
Natalie Pulver <npulver00@gmail.com>
2019-02-04 11:51:14 -0700

    beginning of Navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import "./App.css";
 import routes from "./routes";
-import NavBar from "../src/component/NavBar/NavBar";
-import Footer from "../src/component/Footer/Footer";
+import Footer from "./component/Footer/Footer";
+import NavBar from "./component/NavBar/NavBar";
+
+
+
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-
-        <NavBar_HowItWorks />
-
         <NavBar />
+
+
+
+
         {routes}
         <Footer />
       </div>
