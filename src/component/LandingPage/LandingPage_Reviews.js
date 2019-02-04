466bc163a0149e06fae2f87a5e575c359aac933f
Natalie Pulver <npulver00@gmail.com>
2019-02-04 08:46:02 -0700

    rough draft how it works

---
+++ src/component/LandingPage/LandingPage_Reviews.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./LandingPage.css"
+
+
+const LandingPage_Reviews = () => {
+    return (
+        <div>
+            <div>hsakfjslkjd</div>
+        </div>
+    );
+}
+
+export default LandingPage_Reviews;
