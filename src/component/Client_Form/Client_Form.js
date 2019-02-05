717b469feaa329284b01e2d6b1b4d7910a495967
Justin Horton <justinhorton@Justins-MacBook-Pro.local>
2019-02-05 10:44:42 -0700

    start client_form

---
+++ src/component/Client_Form/Client_Form.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Client_Form extends Component {
+    constructor(){
+        super();
+        this.state = {
+            
+        }
+    }
+    render() {
+        return (
+            <div>
+                Client Form
+            </div>
+        );
+    }
+}
+
+export default Client_Form
--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,13 @@ import Login from "./component/NavBar/Login";
 import LandingPage from "./component/LandingPage/LandingPage";
 import NavBar_HowItWorks from "./component/NavBar/NavBar_HowItWorks";
 import BecomeATasker from "./component/BecomeATasker/BecomeATasker";
+import Client_Form from "./component/Client_Form/Client_Form";
 
 export default (
   <Switch>
     <Route exact path="/" component={LandingPage} />
     <Route path="/how-it-works" component={NavBar_HowItWorks} />
     <Route path="/create-tasker-profile" component={BecomeATasker} />
+    <Route path='/client_form' component={Client_Form} />
   </Switch>
 );
