00545980a93534da169ea021ce714e3508caf66f
Tiffany Zhong <tiffzhong@gmail.com>
2019-02-05 11:43:39 -0700

    commit before merge

--- src/ducks/clientReducer.js
+++ src/ducks/clientReducer.js
@@ -1,22 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
-INITIAL_STATE = {
-    taskType: null,
-}
+const INITIAL_STATE = {
+  taskType: null
+};
 
-const UPDATE_TASK_TYPE = 'UPDATE_TASK_TYPE';
+const UPDATE_TASK_TYPE = "UPDATE_TASK_TYPE";
 
 export function clientReducer(state = INITIAL_STATE, action) {
-    console.log('REDUCER HIT: Action =>', action);
-    switch(action.type) {
-        case UPDATE_TASK_TYPE:
-        return Object.assign({}, state, { taskType: action.payload });
+  console.log("REDUCER HIT: Action =>", action);
+  switch (action.type) {
+    case UPDATE_TASK_TYPE:
+      return Object.assign({}, state, { taskType: action.payload });
 
-
-        default: return state;
-    }
+    default:
+      return state;
+  }
 }
 
 export function updateTaskType(taskType) {
-    axios.post()
+  axios.post();
 }
