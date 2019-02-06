ce624fc125a1dc1ae3ae19bed11cc90bcae05db5
Justin Horton <justinhorton@Justins-MacBook-Pro.local>
2019-02-05 17:02:07 -0700

    making boxes client

--- src/component/Client_Form/Mounting_Form.js
+++ src/component/Client_Form/Mounting_Form.js
@@ -32,8 +32,34 @@ class Mounting_Form extends Component {
         return (
             <div className='form'>
                 Mounting Form
+                <span className='shadow-box'></span>
                 <div className='outer-container'>
-
+                    <div className='question-box'>
+                        <h2>Your Task Start Location</h2>
+                        <input></input>
+                    </div>
+                    <div className='question-box'>
+                        <h2>Duration of Task</h2>
+                        <input></input>
+                    </div>
+                    <div className='question-box'>
+                        <h2>Vehicle Requirements</h2>
+                        <input></input>
+                    </div>
+                    <div className='time-box'>
+                        <div className='small-question-box'>
+                            <h2>Task Start Date</h2>
+                            <input></input>
+                        </div>
+                        <div className='small-question-box'>
+                            <h2>Task Start Time</h2>
+                            <input></input>
+                        </div>
+                    </div>
+                    <div className='question-box'>
+                        <h2>Details of Task</h2>
+                        <input></input>
+                    </div>
                 </div>
             </div>
         );
