6314fa4a9152f6d8d764c58915b4b26b7f42bc10
Justin Horton <justinhorton@Justins-MacBook-Pro.local>
2019-02-07 16:48:42 -0700

    booking task

--- server/controllers/clientController.js
+++ server/controllers/clientController.js
@@ -1,3 +1,23 @@
 module.exports = {
-    
+    bookTask: (req, res) => {
+        console.log('req.body ========>', req.body)
+        const { taskType, locationStart,lat, long, duration, startDate, endDate, taskDetails } = req.body;
+        const db = req.app.get('db');
+        db.client_create([
+            taskType, 
+            locationStart,
+            locationEnd,
+            lat,
+            long, 
+            duration, 
+            startDate, 
+            endDate, 
+            taskDetails
+        ]).then(() => {
+            console.log('You created a new task')
+            
+        }).catch(error => {
+            console.log('Error in Booktask', error);
+        })
+    }
 };
