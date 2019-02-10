c23688c8af84a2b7c40ea286c7799f8b094bea09
Tiffany Zhong <tiffzhong@gmail.com>
2019-02-10 11:51:06 -0700

    styling on landing page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import routes from "./routes";
 import NavBar from "../src/component/NavBar/NavBar";
 import Footer from "../src/component/Footer/Footer";
 import "./reset.css";
+import "./App.css";
 class App extends Component {
   render() {
     return (
