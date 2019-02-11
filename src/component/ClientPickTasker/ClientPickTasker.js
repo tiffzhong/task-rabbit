e584627ddb16f14581a2e0adc026e435ce09b536
Tiffany Zhong <tiffzhong@gmail.com>
2019-02-11 14:31:10 -0700

    fixed client pick tasker file

--- src/component/ClientPickTasker/ClientPickTasker.js
+++ src/component/ClientPickTasker/ClientPickTasker.js
@@ -1,70 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import "./ClientPickTasker.css";
 import axios from "axios";
-import { connect } from 'react-redux';
-import {allTaskerForClient} from "../../ducks/clientReducer";
-
+import { connect } from "react-redux";
+import { allTaskerForClient } from "../../ducks/clientReducer";
 
 class clientPickTasker extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        displaytasker: []
-        }
-    }
-    componentDidMount() {
-     this.displayAllTaskers()
-    }
-    
+  constructor(props) {
+    super(props);
+    this.state = {
+      displaytasker: []
+    };
+  }
+  componentDidMount() {
+    this.displayAllTaskers();
+  }
 
+  displayAllTaskers = () => {
+    this.props.allTaskerForClient().then(response => {
+      console.log(response, "hello wurld");
+      this.setState({
+        displaytasker: this.props.allTaskers
+      });
+    });
+  };
 
-    displayAllTaskers = ()=>{
+  render() {
+    const { allTasker } = this.props;
+    console.log("this.props=>>>>", this.props);
 
-        this.props.allTaskerForClient().then(response=>{
-            this.setState({
-                displaytasker:this.props.taskType
-            })
-        })
-        
+    // const allTaskers = this.state.picktasker.map(taskers=>{
+    //     return(  <div>
+    //         <div> {taskers.tasker_name} </div>
+    //         <div> <img src={taskers.selfie}/> </div>
+    //         <div>{taskers.email} </div>
+    //         <div>{taskers.about} </div>
+    //         <div>hourly amount?</div>
 
-      
-    }
-
-    render() {
-        const { taskType } = this.props
-        console.log("this.props=>>>>", this.props)
-       
-                // const allTaskers = this.state.picktasker.map(taskers=>{
-                //     return(  <div>
-                //         <div> {taskers.tasker_name} </div>
-                //         <div> <img src={taskers.selfie}/> </div>
-                //         <div>{taskers.email} </div>
-                //         <div>{taskers.about} </div>
-                //         <div>hourly amount?</div>
-                        
-                //      </div>
-                //          )
-                // })
-        return (
-            <div className="taskercontainer">
-            PickTasker
-                   {/* <p> {allTaskers} </p> */}
-
-            </div>
-        );
-    }
+    //      </div>
+    //          )
+    // })
+    return (
+      <div className="taskercontainer">
+        PickTasker
+        {/* <p> {allTaskers} </p> */}
+      </div>
+    );
+  }
 }
-const mapStateToProps =(state) =>{
-    const{taskType}= state.client
-    return{
-        taskType
-
-    }
-
-}
+const mapStateToProps = state => {
+  const { allTaskers } = state.client;
+  return {
+    allTaskers
+  };
+};
 
 const mapDispatchToProps = {
-    allTaskerForClient: allTaskerForClient
-}
+  allTaskerForClient: allTaskerForClient
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(clientPickTasker);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(clientPickTasker);
