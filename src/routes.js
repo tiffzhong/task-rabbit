4e0217e35ee0ee36e9a4a13567421f5bc1fbd398
Tiffany Zhong <tiffzhong@gmail.com>
2019-02-13 21:14:22 -0700

    fixed the footer

--- src/routes.js
+++ src/routes.js
@@ -35,8 +35,10 @@ export default (
     <Route path="/edit-client-form/:confirmation_id" component={ClientForm} />
     <Route path="/review" component={Reviews} />
     <Route path="/messages/:client_id" component={MessagesForm} />
-    <Route path='/messages' component={MessagesForm} />
-    <Route path="/messages-personal/:confirmation_id" component={PersonalMessages} />
-    <Route path="/discount" component={EmailModalContent} />
+    <Route path="/messages" component={MessagesForm} />
+    <Route
+      path="/messages-personal/:confirmation_id"
+      component={PersonalMessages}
+    />
   </Switch>
 );
