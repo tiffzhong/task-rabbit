e82a74425c10753d0007eda737bb5a549cd2a406
Tiffany Zhong <tiffzhong@gmail.com>
2019-02-15 15:18:37 -0700

    finished all unit and integration tests

--- server/lib/Users.js
+++ server/lib/Users.js
@@ -16,7 +16,7 @@ module.exports = {
   },
   createClient: (db, bookedTask) => {
     return db.query(
-      "insert into bookedTasks(taskType, locationStart, locationEnd, lat, long, duration, vehicle, startDate, endDate, taskDetails, user_id) values (${taskType}, ${locationStart}, ${locationEnd},${lat}, ${long}, ${duration}, ${vehicle}, ${startDate}, ${endDate}, ${taskDetails}, ${user_id}) returning *;",
+      "insert into bookedTasks(taskType, locationStart, locationEnd, lat, long, duration, vehicle, startDate, endDate, taskDetails, user_id) values (${taskType}, ${locationStart}, ${locationEnd}, ${lat}, ${long}, ${duration}, ${vehicle}, ${startDate}, ${endDate}, ${taskDetails}, ${user_id}) returning *;",
       {
         id: bookedTask.id,
         taskType: bookedTask.taskType,
@@ -30,6 +30,57 @@ module.exports = {
         endDate: bookedTask.endDate,
         taskDetails: bookedTask.taskDetails,
         user_id: bookedTask.user_id
+      }
+    );
+  },
+  createMessage: (db, message) => {
+    return db.query(
+      "insert into messages (confirmation_id, date, poster_id, poster_name, poster_image, message) values(${confirmation_id}, ${date}, ${poster_id}, ${poster_name}, ${poster_image}, ${message}); select * from messages where confirmation_id = 30",
+      {
+        confirmation_id: message.confirmation_id,
+        date: message.date,
+        poster_id: message.poster_id,
+        poster_name: message.poster_name,
+        poster_image: message.poster_image,
+        message: message.message
+      }
+    );
+  },
+  createEmail: (db, email) => {
+    return db.query("insert into nodemailerEmail (email) values (${email});", {
+      email: email.email
+    });
+  },
+
+  updateTaskerProfile: (db, taskerProfile) => {
+    return db.query(
+      "update tasker_profile set tasker_name = ${tasker_name}, email = ${email}, selfie = ${selfie}, phone = ${phone}, location = ${location}, about = ${about}, mounting = ${mounting}, mountingHourly = ${mountingHourly}, delivery = ${delivery}, deliveryHourly = ${deliveryHourly}, yard = ${yard}, yardHourly = ${yardHourly}, home = ${home}, homeHourly = ${homeHourly}, moving = ${moving}, movingHourly= ${movingHourly}, pet = ${pet}, petHourly = ${petHourly}, furniture = ${furniture}, furnitureHourly = ${furnitureHourly}, cleaning = ${cleaning}, cleaningHourly = ${cleaningHourly}, cooking = ${cooking}, cookingHourly = ${cookingHourly} where tasker_id = 'github|39362114' returning *;",
+      {
+        tasker_name: taskerProfile.tasker_name,
+        email: taskerProfile.email,
+        selfie: taskerProfile.selfie,
+        phone: taskerProfile.phone,
+        location: taskerProfile.location,
+        about: taskerProfile.about,
+        mounting: taskerProfile.mounting,
+        mountingHourly: taskerProfile.mountingHourly,
+        delivery: taskerProfile.delivery,
+        deliveryHourly: taskerProfile.deliveryHourly,
+        yard: taskerProfile.yard,
+        yardHourly: taskerProfile.yardHourly,
+        home: taskerProfile.home,
+        homeHourly: taskerProfile.homeHourly,
+        moving: taskerProfile.moving,
+        movingHourly: taskerProfile.movingHourly,
+        pet: taskerProfile.pet,
+        petHourly: taskerProfile.petHourly,
+        furniture: taskerProfile.furniture,
+        furnitureHourly: taskerProfile.furnitureHourly,
+        cleaning: taskerProfile.cleaning,
+        cleaningHourly: taskerProfile.cleaningHourly,
+        cooking: taskerProfile.cooking,
+        cookingHourly: taskerProfile.cookingHourly,
+        tasker_id: "github|39362114"
       }
     );
   }
--- server/lib/Users.test.js
+++ server/lib/Users.test.js
@@ -35,8 +35,8 @@ describe("Unit Tests", () => {
           taskType: "Delivery Service",
           locationStart: "2348 N 7th St, Phoenix, AZ 85006, USA",
           locationEnd: "3450 N 7th St, Phoenix, AZ 85014, USA",
-          lat: 33.488093,
-          long: -112.065685,
+          lat: "33.488093",
+          long: "-112.065685",
           duration: "Est. 1 hr",
           vehicle: "Truck",
           startDate: "2019-02-16T17:30:25.473Z",
@@ -45,6 +45,67 @@ describe("Unit Tests", () => {
           user_id: "github|28162481"
         };
         return UsersData.createClient(bookedTasks, bookedTask);
+      });
+    }),
+    describe("Create Message", () => {
+      it("Should create a new message", () => {
+        const messages = {
+          query: sinon.mock().withArgs(sinon.match.string)
+        };
+        const message = {
+          confirmation_id: 30,
+          date: "leh dateh",
+          poster_id: "githubeh ideh",
+          poster_name: "nameh",
+          poster_image: "imageh",
+          message: "messageeh"
+        };
+        return UsersData.createMessage(messages, message);
+      });
+    }),
+    describe("Should Successfully Insert into Nodemailer Table", () => {
+      it("Should insert into nodemailer", () => {
+        const emails = {
+          query: sinon.mock().withArgs(sinon.match.string)
+        };
+        const email = {
+          email: "This is the email"
+        };
+        return UsersData.createEmail(emails, email);
+      });
+    }),
+    describe("Update tasker profile", () => {
+      it("Should update tasker profile", () => {
+        const taskerProfiles = {
+          query: sinon.mock().withArgs(sinon.match.string)
+        };
+        const taskerProfile = {
+          tasker_name: "Tasker Name",
+          email: "Tasker Email",
+          selfie: "text",
+          phone: "text",
+          location: "text",
+          about: "text",
+          mounting: false,
+          mountingHourly: "text",
+          delivery: false,
+          deliveryHourly: "text",
+          yard: false,
+          yardHourly: "text",
+          home: false,
+          homeHourly: "text",
+          moving: false,
+          movingHourly: "text",
+          pet: false,
+          petHourly: "text",
+          furniture: false,
+          furnitureHourly: "text",
+          cleaning: true,
+          cleaningHourly: "text",
+          cooking: true,
+          cookingHourly: "text"
+        };
+        return UsersData.updateTaskerProfile(taskerProfiles, taskerProfile);
       });
     });
 });
@@ -73,6 +134,7 @@ describe("Integration Tests", () => {
       });
     });
   });
+
   describe("Creating user from auth0 login", () => {
     it("Should create a user from auth0 login", () => {
       const user = {
@@ -93,6 +155,7 @@ describe("Integration Tests", () => {
       });
     });
   });
+
   describe("Creating a new booked task", () => {
     it("Should create a new booked task", () => {
       const bookedTask = {
@@ -112,19 +175,118 @@ describe("Integration Tests", () => {
         expect(newBookedTask.length).not.toEqual(0);
         expect(newBookedTask[0]).toMatchObject({
           id: expect.any(Number),
-          taskType: expect.any(String),
-          locationStart: expect.any(String),
-          locationEnd: expect.any(String),
+          tasktype: expect.any(String),
+          locationstart: expect.any(String),
+          locationend: expect.any(String),
           lat: expect.any(String),
           long: expect.any(String),
           duration: expect.any(String),
           vehicle: expect.any(String),
-          startDate: expect.any(String),
-          endDate: expect.any(String),
-          taskDetails: expect.any(String),
+          startdate: expect.any(String),
+          enddate: expect.any(String),
+          taskdetails: expect.any(String),
           user_id: expect.any(String)
         });
       });
+    });
+  });
+
+  describe("Creating a new Message", () => {
+    it("Should create a new message", () => {
+      const message = {
+        confirmation_id: 30,
+        date: "leh dateh",
+        poster_id: "githubeh ideh",
+        poster_name: "nameh",
+        poster_image: "imageh",
+        message: "messageeh"
+      };
+      return UsersData.createMessage(db, message).then(newMessage => {
+        expect(newMessage.length).not.toEqual(0);
+        expect(newMessage[0]).toMatchObject({
+          confirmation_id: expect.any(Number),
+          date: expect.any(String),
+          poster_id: expect.any(String),
+          poster_name: expect.any(String),
+          poster_image: expect.any(String),
+          message: expect.any(String)
+        });
+      });
+    });
+  });
+
+  describe("Inserting into Nodemailer table", () => {
+    it("Should create a nodemailer", () => {
+      const email = {
+        email: "This is the email"
+      };
+      return UsersData.createEmail(db, email).then(newEmail => {
+        expect(newEmail.length).toEqual(0);
+      });
+    });
+  });
+
+  describe("editing tasker profile", () => {
+    it("Should edit the tasker profile", () => {
+      const taskerProfile = {
+        tasker_name: "Tasker Name",
+        email: "Tasker Email",
+        selfie: "text",
+        phone: "4801234566",
+        location: "text",
+        about: "text",
+        mounting: false,
+        mountingHourly: "text",
+        delivery: false,
+        deliveryHourly: "text",
+        yard: false,
+        yardHourly: "text",
+        home: false,
+        homeHourly: "text",
+        moving: false,
+        movingHourly: "text",
+        pet: false,
+        petHourly: "text",
+        furniture: false,
+        furnitureHourly: "text",
+        cleaning: true,
+        cleaningHourly: "text",
+        cooking: true,
+        cookingHourly: "text",
+        tasker_id: "text"
+      };
+      return UsersData.updateTaskerProfile(db, taskerProfile).then(
+        newTaskerProfile => {
+          expect(newTaskerProfile.length).not.toEqual(0);
+          expect(newTaskerProfile[0]).toMatchObject({
+            tasker_id: expect.any(String),
+            tasker_name: expect.any(String),
+            email: expect.any(String),
+            selfie: expect.any(String),
+            phone: expect.any(String),
+            location: expect.any(String),
+            about: expect.any(String),
+            mounting: expect.any(Boolean),
+            mountinghourly: expect.any(String),
+            delivery: expect.any(Boolean),
+            deliveryhourly: expect.any(String),
+            yard: expect.any(Boolean),
+            yardhourly: expect.any(String),
+            home: expect.any(Boolean),
+            homehourly: expect.any(String),
+            moving: expect.any(Boolean),
+            movinghourly: expect.any(String),
+            pet: expect.any(Boolean),
+            pethourly: expect.any(String),
+            furniture: expect.any(Boolean),
+            furniturehourly: expect.any(String),
+            cleaning: expect.any(Boolean),
+            cleaninghourly: expect.any(String),
+            cooking: expect.any(Boolean),
+            cookinghourly: expect.any(String)
+          });
+        }
+      );
     });
   });
 });
