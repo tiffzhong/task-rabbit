333eb6fc258424d26fc826dc60a9841991aec776
Justin Horton <justinhorton@justins-mbp.lan>
2019-02-17 13:58:13 -0700

    pulled old index.js for server out of github

--- server/index.js
+++ server/index.js
@@ -8,7 +8,6 @@ dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
-app.use(express.static(`__dirname/../build`));
 
 massive(process.env.CONNECTION_STRING)
   .then(database => {
@@ -17,11 +16,6 @@ massive(process.env.CONNECTION_STRING)
   .catch(error => {
     console.log("error with massive", error);
   });
-
-const path = require("path");
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
-});
 
 app.use(
   session({
@@ -41,9 +35,7 @@ app.use(
 const authController = require("./controllers/authContoller");
 const taskerController = require("./controllers/taskerController");
 const clientController = require("./controllers/clientController");
-const confirmationController = require("./controllers/confirmationController");
-const messagesController = require("./controllers/messagesController");
-const stripeController = require("./controllers/stripeController");
+
 //Endpoints
 //Auth
 app.get("/auth/callback", authController.login);
@@ -52,33 +44,11 @@ app.post("/auth/logout", authController.logout);
 
 //Tasker
 app.post("/api/tasker", taskerController.createProfile);
-app.get("/api/tasker", taskerController.getAllTaskers);
 app.get("/api/tasker/:tasker_id", taskerController.getProfile);
 app.put("/api/tasker/:tasker_id", taskerController.editProfile);
 
 //Client
-app.post("/api/client", clientController.bookTask);
-app.put("/api/client/:confirmation_id", clientController.editTask);
-app.get("/api/pickatasker", clientController.allTaskers);
-app.get("/api/client/:client_id", clientController.getClient);
-
-//Confirmation
-app.post("/api/confirmed", confirmationController.createConfirmed);
-app.get(
-  "/api/confirmed/:confirmation_id",
-  confirmationController.getConfirmation
-);
-//nodemailer
-app.post("/api/email", clientController.nodemailerEmail);
-
-//stripe
-app.post("/api/stripe", stripeController.stripeCheckout);
-
-//Messages
-app.get("/messages/:id", messagesController.getClientMessages);
-app.get("/api/personal/messages/:id", messagesController.getMessages);
-app.post("/api/messages", messagesController.createMessage);
-// app.get('/api/messages', messagesController.getMessages);
+app.post('/api/client', clientController.bookTask);
 
 const PORT = 4000;
 app.listen(PORT, () => {
